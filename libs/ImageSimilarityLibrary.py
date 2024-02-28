d93abf848735f631e4eb180c98e09a2faaa2ec28
Marko Korkiakoski <marko.korkiakoski@hiddentrail.com>
2024-02-28 17:01:03 +0200

    Renamed all private functions to start with _

--- libs/ImageSimilarityLibrary.py
+++ libs/ImageSimilarityLibrary.py
@@ -22,15 +22,15 @@ class ImageSimilarityLibrary:
 
     @not_keyword
     def _compare_images(self, expected_image_path, actual_image_url):
-        expected_image = read_image(expected_image_path)
-        actual_image = read_image_from_url(actual_image_url)
-        actual_image = resize_image(actual_image, image_size(expected_image))
+        expected_image = _read_image(expected_image_path)
+        actual_image = _read_image_from_url(actual_image_url)
+        actual_image = _resize_image(actual_image, _image_size(expected_image))
         # write_image_to_file(actual_image, "actual_image.jpg")  # just debug
 
-        diff1 = compare_images_opencv(actual_image, expected_image)
-        diff2 = compare_images_pillow(actual_image, expected_image)
-        diff3 = compare_images_scikit(actual_image, expected_image)
-        diff4 = compare_images_imagehash(actual_image, expected_image)
+        diff1 = _compare_images_opencv(actual_image, expected_image)
+        diff2 = _compare_images_pillow(actual_image, expected_image)
+        diff3 = _compare_images_scikit(actual_image, expected_image)
+        diff4 = _compare_images_imagehash(actual_image, expected_image)
 
         print("similarity opencv:   ", diff1)
         print("similarity pillow:   ", diff2)
@@ -39,14 +39,14 @@ class ImageSimilarityLibrary:
 
         return diff1, diff2, diff3, diff4
 
-def read_image(file_path):
+def _read_image(file_path):
     image = cv2.imread(file_path)
     if image is None:
         raise FileNotFoundError(f"Image not found at {file_path}")
 
     return image
 
-def read_image_from_url(url):
+def _read_image_from_url(url):
     resp = urllib.request.urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
@@ -55,19 +55,19 @@ def read_image_from_url(url):
 
     return image
 
-def write_image_to_file(cv2_image, file_path):
+def _write_image_to_file(cv2_image, file_path):
     success = cv2.imwrite(file_path, cv2_image)
     if not success:
         raise IOError("Failed to write image to file at " + file_path)
 
-def image_size(image):
+def _image_size(image):
     height, width = image.shape[:2]
 
     return (width, height)
 
-def resize_image(image, size, keep_aspect_ratio=False):
+def _resize_image(image, size, keep_aspect_ratio=False):
     if keep_aspect_ratio:
-        (height, width) = image.shape[:2]
+        (height, width) = _image_size(image)
         (new_width, new_height) = size
         ratio_w = new_width / width
         ratio_h = new_height / height
@@ -79,7 +79,7 @@ def resize_image(image, size, keep_aspect_ratio=False):
 
     return resized_image
 
-def compare_images_opencv(image1, image2):
+def _compare_images_opencv(image1, image2):
     gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
     mse = np.mean((gray1 - gray2) ** 2)
@@ -88,7 +88,7 @@ def compare_images_opencv(image1, image2):
     else:
         return 1 / (1 + mse)
 
-def compare_images_pillow(image1, image2):
+def _compare_images_pillow(image1, image2):
     np_img1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     np_img2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
     difference = np.abs(np_img1 - np_img2)
@@ -96,23 +96,14 @@ def compare_images_pillow(image1, image2):
 
     return score
 
-def compare_images_scikit(image1, image2):
+def _compare_images_scikit(image1, image2):
     gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
     score, _ = ssim(gray1, gray2, full=True)
 
     return score
 
-def compare_images_imagehash_OLD(image1, image2):
-    hash1 = imagehash.average_hash(image1)
-    hash2 = imagehash.average_hash(image2)
-    difference = hash1 - hash2
-    max_difference = max(len(hash1.hash)**2, len(hash2.hash)**2)
-    similarity = 1 - difference / max_difference
-
-    return similarity
-
-def compare_images_imagehash(image1, image2):
+def _compare_images_imagehash(image1, image2):
     pil_image1 = Image.fromarray(cv2.cvtColor(image1, cv2.COLOR_BGR2RGB))
     pil_image2 = Image.fromarray(cv2.cvtColor(image2, cv2.COLOR_BGR2RGB))
     hash1 = imagehash.average_hash(pil_image1)
